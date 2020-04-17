fcbe8275fcbb11c63b4af503d77ef45ab1ba6c74
frankluongo <frank@frankluongo.com>
2020-04-17 13:16:47 -0400

    Added the text and updated some formatting

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -6,13 +6,23 @@ import LilyPhoto from "../LilyPhoto/LilyPhoto"
 const Message = () => {
   return (
     <section className="message">
-      <h1 className="message__heading">
-        Unfortunately, We're Postponing Lily's Birthday party
-      </h1>
+      <h1 className="message__heading">Good times come to those who wait</h1>
       <LilyPhoto />
       <p className="message__text">
-        During these unprecendented times, we have indefnitely suspended the
-        birthday party for our little baby Lily.
+        Dear Family and Friends,
+        <br />
+        <br />
+        Regrettably due to the current COVID-19 pandemic, we have decided to
+        postpone Lily's 1st Birthday party until further notice. As much as we
+        wish we could all celebrate together, nothing is more important to us
+        than the safety and health of our beloved friends and family. We look
+        forward to the day when we can all celebrate together with peace of
+        mind. We will resend invitations when we decide to reschedule Lily's
+        celebration.
+        <br />
+        <br />
+        With love, <br />
+        Paul, Kristin, and Lily
       </p>
     </section>
   )
